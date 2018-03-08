10b7950556fc09b4e370089029d5c054f30c52f5
nogoegst <nogoegst@users.noreply.github.com>
2018-03-08 19:28:02 +0300

    Fix image size calculation in 'list' command (#61)

--- client/list.go
+++ client/list.go
@@ -6,10 +6,18 @@ import (
 	"strings"
 
 	"github.com/containerd/containerd/images"
+	"github.com/containerd/containerd/platforms"
 )
 
+// ListedImage represents an image structure returuned from ListImages.
+// It extends containerd/images.Image with extra fields.
+type ListedImage struct {
+	images.Image
+	ContentSize int64
+}
+
 // ListImages returns the images from the image store.
-func (c *Client) ListImages(ctx context.Context, filters ...string) ([]images.Image, error) {
+func (c *Client) ListImages(ctx context.Context, filters ...string) ([]ListedImage, error) {
 	// Create the worker opts.
 	opt, err := c.createWorkerOpt()
 	if err != nil {
@@ -22,5 +30,13 @@ func (c *Client) ListImages(ctx context.Context, filters ...string) ([]images.Im
 		return nil, fmt.Errorf("listing images with filters (%s) failed: %v", strings.Join(filters, ", "), err)
 	}
 
-	return i, nil
+	listedImages := []ListedImage{}
+	for _, image := range i {
+		size, err := image.Size(ctx, opt.ContentStore, platforms.Default())
+		if err != nil {
+			return nil, fmt.Errorf("calculating size of image %s failed: %v", image.Name, err)
+		}
+		listedImages = append(listedImages, ListedImage{Image: image, ContentSize: size})
+	}
+	return listedImages, nil
 }
