e6b9835f84697b64ed693da2728335ec82c9024e
Mauricio Ugaz <pierre.ugazm@gmail.com>
2018-05-25 12:03:43 -0400

    Check that root is not symlink (#48)

--- executor/runc/executor.go
+++ executor/runc/executor.go
@@ -52,7 +52,14 @@ func New(root string, unprivileged, noMount bool) (executor.Executor, error) {
 		return nil, err
 	}
 
-	// TODO: check that root is not symlink to fail early
+	// Check that root is not symlink to fail early
+	rootInfo, err := os.Lstat(root)
+	if err != nil {
+		return nil, err
+	}
+	if rootInfo.Mode() & os.ModeSymlink != 0 {
+		return nil, fmt.Errorf("%s is a symlink", root)
+    	}
 
 	runtime := &runc.Runc{
 		Log:          filepath.Join(root, "runc-executor-log.json"),
